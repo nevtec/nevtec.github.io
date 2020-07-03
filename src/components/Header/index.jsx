342be8b9fa33be569c1a6a60623836acff55cebc
Everton F. Xavier <evertonfxavier@gmail.com>
2020-07-03 12:09:41 -0300

    adjusts in alias and add fonts e img in static

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,35 @@
-import React, { useEffect } from "react";
-import Aos from "aos";
-import scrollTo from 'gatsby-plugin-smoothscroll';
+import React, { useEffect } from "react"
+import Aos from "aos"
+import scrollTo from "gatsby-plugin-smoothscroll"
 
-import Logo from '@components/Logo/';
-import Menu from '@components/Menu/';
-import SocialMedia from '@components/SocialMedia';
-import ScrollDown from '@components/ScrollDown/';
-import Title from '@components/common/Title';
-import Description from '@components/common/Description';
-import BtnStyled from '@components/common/BtnStyles';
+import Logo from "@components/Logo/"
+import Menu from "@components/Menu/"
+import SocialMedia from "@components/SocialMedia"
+import ScrollDown from "@components/ScrollDown/"
+import Title from "@components/common/Title"
+import Description from "@components/common/Description"
+import BtnStyled from "@components/common/BtnStyles"
 
-import { HeaderWrapper, ContainerHeader, PositionLogo, ContentHeader, BtnWrapper, ScrollWrapper } from './styled';
+import {
+  HeaderWrapper,
+  ContainerHeader,
+  PositionLogo,
+  ContentHeader,
+  BtnWrapper,
+  ScrollWrapper,
+} from "./styled"
 
-const scrollToIndex = (Header) => () => scrollTo(Header);
+const scrollToIndex = Header => () => scrollTo(Header)
 
-const scrollToAbout = (About) => () => scrollTo(About);
+const scrollToAbout = About => () => scrollTo(About)
 
 const Header = () => {
-
   useEffect(() => {
-    Aos.init({ duration: 2000, offset : 210, once: true, });
-  }, []);
+    Aos.init({ duration: 2000, offset: 210, once: true })
+  }, [])
 
   return (
     <HeaderWrapper id="Header">
-
       <ContainerHeader>
         <Menu />
         <SocialMedia />
@@ -34,33 +39,29 @@ const Header = () => {
         </PositionLogo>
 
         <ContentHeader data-aos="fade-up">
-          <Description variant="primary">
-            WELCOME TO NEVTEC
-          </Description>
+          <Description variant="primary">WELCOME TO NEVTEC</Description>
           <Title variant="primary">
-            We are a creative group of nevinhosos who design
-            influential brands and digital experiences.
+            We are a creative group of nevinhosos who design influential brands
+            and digital experiences.
           </Title>
 
           <BtnWrapper>
-            <BtnStyled variant="primary" onClick={scrollToIndex('#Header')}>
+            <BtnStyled variant="primary" onClick={scrollToIndex("#Header")}>
               start a project
             </BtnStyled>
 
-            <BtnStyled variant="primary" onClick={scrollToAbout('#About')}>
+            <BtnStyled variant="primary" onClick={scrollToAbout("#About")}>
               more about us
             </BtnStyled>
           </BtnWrapper>
         </ContentHeader>
-
       </ContainerHeader>
-      
+
       <ScrollWrapper>
         <ScrollDown />
       </ScrollWrapper>
-
     </HeaderWrapper>
   )
-};
+}
 
-export default Header;
+export default Header
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,36 +1,36 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
-import img from '@images/back.jpg';
+import img from "@images/back.jpg"
 
 export const HeaderWrapper = styled.header`
-    width: 100%;
-    background-image: url(${img});
-    background-repeat: no-repeat;
-    background-position: center;
-    background-attachment: fixed;
-    background-size: cover;
-    opacity: .9;
-    display: flex;
-    padding-bottom: 138px;
+  width: 100%;
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+  background-size: cover;
+  opacity: 0.9;
+  display: flex;
+  padding-bottom: 138px;
 `
 
 export const ContainerHeader = styled.div`
-    ${ ({ theme }) => theme.setContainer()};
+  ${({ theme }) => theme.setContainer()};
 `
 
 export const PositionLogo = styled.div`
-    width: 100%;
-    max-width: 130px;
-    margin-right: auto;
-    padding: 38px 0;
+  width: 100%;
+  max-width: 130px;
+  margin-right: auto;
+  padding: 38px 0;
 `
 
 export const ContentHeader = styled.div`
-    width: 100%;
-    max-width: 1000px;
-    padding-top: 114px;
+  width: 100%;
+  max-width: 1000px;
+  padding-top: 114px;
 
-    ${({ theme }) => theme.media.phone`
+  ${({ theme }) => theme.media.phone`
         text-align: center;
         text-align: -webkit-center;
         padding-top: 30px;
@@ -38,17 +38,17 @@ export const ContentHeader = styled.div`
 `
 
 export const BtnWrapper = styled.div`
-    max-width: 450px;
-    display: flex;
-    justify-content: space-between;
-    padding-top: 50px;
+  max-width: 450px;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 50px;
 
-    ${({ theme }) => theme.media.phone`
+  ${({ theme }) => theme.media.phone`
         flex-direction: column;
         padding-top: 10px;
-    `} 
+    `}
 `
 
 export const ScrollWrapper = styled.div`
-    position: relative;
+  position: relative;
 `
